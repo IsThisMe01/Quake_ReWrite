6538de8180ffd38cb9fa42c4d22c6215a817b91b
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2025-09-20 13:50:03 -0700

    Update bundled.lua

--- bundled.lua
+++ bundled.lua
@@ -1,5 +1,5 @@
 -- ++++++++ WAX BUNDLED DATA BELOW ++++++++ --
-
+-- bb
 -- Will be used later for getting flattened globals
 local ImportGlobals
 
@@ -7634,3 +7634,4 @@ end
 
 -- AoT adjustment: Load init module (MainModule behavior)
 return LoadScript(RealObjectRoot:GetChildren()[1])
+
